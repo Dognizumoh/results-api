779eaa7b3308082e70b59a406fd5606a84ca2da0
Dognizumoh <idafolo16@gmail.com>
2022-04-06 14:44:53 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,15 @@
+const results=[
+    {name:"Windy Dari", class:"Form One", marks:8},
+    {name:"Winnie Dery", class:"Form One", marks:9},
+    {name:"Jemila Wilson", class:"Form One", marks:10},
+    {name:"Jabs Ocansey", class:"Form One", marks:7},
+    {name:"Folo Nabaar", class:"Form One", marks:10},
+    {name:"Sendi Taale", class:"Form One", marks:9},
+    {name:"Sandy Tassun", class:"Form One", marks:8},
+    {name:"Vera Taya", class:"Form One", marks:5},
+    {name:"Sidney Tommy", class:"Form One", marks:6},
+    {name:"Sesi Aziz", class:"Form One", marks:2},
+
+]
+
+module.exports=results
